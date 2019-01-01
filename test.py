9a4ceba457f9a1e04879ccefaa8b7cd4f0edb5fd
Martin Scavnicky <martin.scavnicky@gmail.com>
2019-01-01 14:48:22 +0100

    Cover all commands with unittests.

--- test.py
+++ test.py
@@ -1,16 +1,97 @@
+import unittest
 import logging
 from hypotonic import Hypotonic
 
 logger = logging.getLogger('hypotonic')
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
 
-print(
-  list(
-    Hypotonic()
-      .get('https://www.bergfex.com/slovakia/top10/')
-      .find('.section-left div:nth-child(1) a:nth-child(1)')
-      .set('resort')
-      .follow('@href')
-      .set({'size': '.big'})
-      .data()))
+
+class TestHypotonic(unittest.TestCase):
+  def setUp(self):
+    self.hypotonic = Hypotonic()
+    self.hypotonic.get('http://books.toscrape.com/')
+
+  def test_find_with_xpath(self):
+    data = list(
+      self.hypotonic
+        .find(
+        '//*[contains(concat( " ", @class, " " ), concat( " ", "nav-list", " " ))]//ul//a')
+        .set('category')
+        .data())
+
+    self.assertEqual(50, len(data))
+    self.assertTrue({'category': 'Romance'} in data)
+
+  def test_find_with_css(self):
+    data = list(
+      self.hypotonic
+        .find('.nav-list ul a')
+        .set('category')
+        .data())
+
+    self.assertEqual(50, len(data))
+    self.assertIn({'category': 'Romance'}, data)
+
+  def test_set_with_str(self):
+    data = list(
+      self.hypotonic
+        .find('.h1 a')
+        .set('title')
+        .data())
+
+    self.assertEqual([{'title': 'Books to Scrape'}], data)
+
+  def test_set_with_dict(self):
+    data = list(
+      self.hypotonic
+        .find('li article')
+        .set({'title': 'h3 a',
+              'price': '.price_color',
+              'availability': 'p.availability'})
+        .data())
+
+    self.assertEqual(20, len(data))
+    self.assertIn({'title': 'Sharp Objects',
+                   'price': '£47.82',
+                   'availability': 'In stock'}, data)
+
+  def test_follow(self):
+    data = list(
+      self.hypotonic
+        .find('.product_pod h3 a')
+        .follow('@href')
+        .find('h1')
+        .set('title')
+        .data())
+
+    self.assertEqual(20, len(data))
+    self.assertIn({'title': 'Sharp Objects'}, data)
+
+  def test_filter(self):
+    data = list(
+      self.hypotonic
+        .find('li article')
+        .filter('.Five')
+        .set({'title': 'h3 a'})
+        .data())
+
+    self.assertEqual(4, len(data))
+    self.assertIn({'title': 'Set Me Free'}, data)
+    self.assertNotIn({'title': 'Sharp Objects'}, data)
+
+  def test_match(self):
+    data = list(
+      self.hypotonic
+        .find('.product_pod h3 a')
+        .match('[tT]he')
+        .set('title')
+        .data())
+
+    self.assertEqual(9, len(data))
+    self.assertIn({'title': 'Tipping the Velvet'}, data)
+    self.assertNotIn({'title': 'Sharp Objects'}, data)
+
+
+if __name__ == '__main__':
+  unittest.main()
