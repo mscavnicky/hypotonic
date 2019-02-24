102159a5284914d31bf3193f4cbb7d515332f484
Martin Scavnicky <martin.scavnicky@gmail.com>
2019-02-24 12:16:52 +0100

    Packageify.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='hypotonic',
+      version='0.0.1',
+      description='Fast asynchronous web scraper with minimalist API.',
+      classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing :: Markup :: HTML'
+      ],
+      url='https://github.com/mscavnicky/hypotonic',
+      author='Martin Scavnicky',
+      author_email='martin.scavnicky@gmail.com',
+      license='MIT',
+      packages=find_packages(exclude=['tests', 'tests.*']),
+      install_requires=['aiohttp', 'lxml', 'cssselect'],
+      include_package_data=True,
+      zip_safe=False)
