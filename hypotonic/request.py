c4582f746e0d7546b667058c9e78d5504a433573
Martin Scavnicky <martin.scavnicky@gmail.com>
2020-09-19 01:43:58 +0200

    Add retries with exponential backoff to every request.

--- hypotonic/request.py
+++ hypotonic/request.py
@@ -1,11 +1,25 @@
-"""Convenience methods for asynchronous HTTP requests."""
+"""
+Convenience methods for asynchronous HTTP requests.
+
+For performance reasons, we are utilizing aiohttp with keepalive connections [1].
+Long-running connections get closed eventually, and aiohttp session throws
+an exception to signal those. Exponential backoff is employed to resolve those.
+
+References:
+[1] https://github.com/aio-libs/aiohttp/issues/850#issuecomment-210883478
+"""
+
+import backoff
+import aiohttp
 
 
+@backoff.on_exception(backoff.expo, aiohttp.ClientConnectionError, max_tries=3)
 async def get(session, url, params=None):
   async with session.get(url, params=params) as response:
     return response.url, response.content_type, await response.text()
 
 
+@backoff.on_exception(backoff.expo, aiohttp.ClientConnectionError, max_tries=3)
 async def post(session, url, payload=None):
   async with session.post(url, data=payload) as response:
     return response.url, response.content_type, await response.text()
