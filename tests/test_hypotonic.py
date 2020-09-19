8f3112edc8984b7d7f82edd8e7fbf9d08dcd30b2
Martin Scavnicky <martin.scavnicky@gmail.com>
2020-09-19 19:18:55 +0200

    Run tests asynchronously by default.

--- tests/test_hypotonic.py
+++ tests/test_hypotonic.py
@@ -14,7 +14,124 @@ vcr = VCR(cassette_library_dir='./tests/cassettes')
 
 
 class TestHypotonic(aiounittest.AsyncTestCase):
-  async def test_async_run(self):
+  def test_synchronous_run(self):
+    data, errors = (
+      Hypotonic('http://books.toscrape.com/')
+        .find('.nav-list ul a')
+        .set('category')
+        .data())
+
+    self.assertFalse(errors)
+    self.assertEqual(50, len(data))
+
+  async def test_get_unreachable_url(self):
+    data, errors = await (
+      Hypotonic()
+        .get('https://non-existing-url.com')
+        .run()
+    )
+
+    self.assertEqual(0, len(data))
+    self.assertEqual(1, len(errors))
+    self.assertIsInstance(errors[0][2], aiohttp.ClientConnectorError)
+
+  async def test_get_redirect(self):
+    data, errors = await (
+      Hypotonic()
+        .get('https://httpstat.us/301')
+        .find('h1')
+        .set('title')
+        .run()
+    )
+
+    self.assertEqual(0, len(errors))
+    self.assertEqual([{'title': 'httpstat.us'}], data)
+
+  async def test_get_not_found(self):
+    data, errors = await (
+      Hypotonic()
+        .get('https://httpstat.us/404')
+        .run()
+    )
+
+    self.assertEqual(0, len(data))
+    self.assertEqual(1, len(errors))
+    self.assertIsInstance(errors[0][2], aiohttp.ClientResponseError)
+
+  async def test_get_internal_server_error(self):
+    data, errors = await (
+      Hypotonic()
+        .get('https://httpstat.us/500')
+        .run()
+    )
+
+    self.assertEqual(0, len(data))
+    self.assertEqual(1, len(errors))
+    self.assertIsInstance(errors[0][2], aiohttp.ClientResponseError)
+
+
+  async def test_get_with_params(self):
+    data, errors = await (
+      Hypotonic()
+        .get('http://testing-ground.scraping.pro/textlist', {'ver': '3'})
+        .find('h1')
+        .set('title')
+        .run()
+    )
+
+    self.assertFalse(errors)
+    self.assertEqual(1, len(data))
+    self.assertIn({'title': 'TEXT LIST (version 3)'}, data)
+
+  async def test_get_multiple_urls(self):
+    data, errors = await (
+      Hypotonic()
+        .get([f"http://quotes.toscrape.com/page/{page}/" for page in range(1, 11)])
+        .find('.quote .text')
+        .set('quote')
+        .run()
+    )
+
+    self.assertFalse(errors)
+    self.assertEqual(100, len(data))
+
+  async def test_post(self):
+    data, errors = await (
+      Hypotonic()
+        .post('http://quotes.toscrape.com/login',
+              {'username': 'admin', 'password': 'admin'})
+        .find('.header-box .col-md-4 a')
+        .set('label')
+        .run())
+
+    self.assertFalse(errors)
+    self.assertEqual([{'label': 'Logout'}], data)
+
+  async def test_find_missing_element(self):
+    data, errors = await (
+      Hypotonic()
+        .get('http://books.toscrape.com/')
+        .find('h11 fake-class')
+        .set('title')
+        .run()
+    )
+
+    self.assertFalse(errors)
+    self.assertFalse(data)
+
+  async def test_find_with_invalid_selector(self):
+    data, errors = await (
+      Hypotonic()
+        .get('http://books.toscrape.com/')
+        .find('.#./')
+        .set('title')
+        .run()
+    )
+
+    self.assertFalse(data)
+    self.assertEqual(1, len(errors))
+
+  async def test_find_with_css(self):
     data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('.nav-list ul a')
@@ -23,155 +140,38 @@ class TestHypotonic(aiounittest.AsyncTestCase):
 
     self.assertFalse(errors)
     self.assertEqual(50, len(data))
-
-  def test_get_unreachable_url(self):
-    data, errors = (
-      Hypotonic()
-        .get('https://non-existing-url.com')
-        .data()
-    )
-
-    self.assertEqual(0, len(data))
-    self.assertEqual(1, len(errors))
-    self.assertIsInstance(errors[0][2], aiohttp.ClientConnectorError)
-
-  def test_get_redirect(self):
-    data, errors = (
-      Hypotonic()
-        .get('https://httpstat.us/301')
-        .find('h1')
-        .set('title')
-        .data()
-    )
-
-    self.assertEqual(0, len(errors))
-    self.assertEqual([{'title': 'httpstat.us'}], data)
-
-  def test_get_not_found(self):
-    data, errors = (
-      Hypotonic()
-        .get('https://httpstat.us/404')
-        .data()
-    )
-
-    self.assertEqual(0, len(data))
-    self.assertEqual(1, len(errors))
-    self.assertIsInstance(errors[0][2], aiohttp.ClientResponseError)
-
-  def test_get_internal_server_error(self):
-    data, errors = (
-      Hypotonic()
-        .get('https://httpstat.us/500')
-        .data()
-    )
-
-    self.assertEqual(0, len(data))
-    self.assertEqual(1, len(errors))
-    self.assertIsInstance(errors[0][2], aiohttp.ClientResponseError)
-
-
-  def test_get_with_params(self):
-    data, errors = (
-      Hypotonic()
-        .get('http://testing-ground.scraping.pro/textlist', {'ver': '3'})
-        .find('h1')
-        .set('title')
-        .data()
-    )
-
-    self.assertFalse(errors)
-    self.assertEqual(1, len(data))
-    self.assertIn({'title': 'TEXT LIST (version 3)'}, data)
-
-  def test_get_multiple_urls(self):
-    data, errors = (
-      Hypotonic()
-        .get([f"http://quotes.toscrape.com/page/{page}/" for page in range(1, 11)])
-        .find('.quote .text')
-        .set('quote')
-        .data()
-    )
-
-    self.assertFalse(errors)
-    self.assertEqual(100, len(data))
-
-  def test_post(self):
-    data, errors = (
-      Hypotonic()
-        .post('http://quotes.toscrape.com/login',
-              {'username': 'admin', 'password': 'admin'})
-        .find('.header-box .col-md-4 a')
-        .set('label')
-        .data())
-
-    self.assertFalse(errors)
-    self.assertEqual([{'label': 'Logout'}], data)
-
-  def test_find_missing_element(self):
-    data, errors = (
-      Hypotonic()
-        .get('http://books.toscrape.com/')
-        .find('h11 fake-class')
-        .set('title')
-        .data()
-    )
-
-    self.assertFalse(errors)
-    self.assertFalse(data)
-
-  def test_find_with_invalid_selector(self):
-    data, errors = (
-      Hypotonic()
-        .get('http://books.toscrape.com/')
-        .find('.#./')
-        .set('title')
-        .data()
-    )
-
-    self.assertFalse(data)
-    self.assertEqual(1, len(errors))
-
-  def test_find_with_css(self):
-    data, errors = (
-      Hypotonic('http://books.toscrape.com/')
-        .find('.nav-list ul a')
-        .set('category')
-        .data())
-
-    self.assertFalse(errors)
-    self.assertEqual(50, len(data))
     self.assertIn({'category': 'Romance'}, data)
 
-  def test_set_with_str(self):
-    data, errors = (
+  async def test_set_with_str(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('.h1 a')
         .set('title')
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual([{'title': 'Books to Scrape'}], data)
 
-  def test_set_with_array(self):
-    data, errors = (
+  async def test_set_with_array(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .set({'title': '.h1 a',
               'categories': ['.nav-list ul a']})
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(1, len(data))
     self.assertEqual(50, len(data[0]['categories']))
     self.assertIn('Romance', data[0]['categories'])
 
-  def test_set_with_dict(self):
-    data, errors = (
+  async def test_set_with_dict(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('li article')
         .set({'title': 'h3 a',
               'price': '.price_color',
               'availability': 'p.availability'})
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(20, len(data))
@@ -179,13 +179,13 @@ class TestHypotonic(aiounittest.AsyncTestCase):
                    'price': '£47.82',
                    'availability': 'In stock'}, data)
 
-  def test_set_attr(self):
-    data, errors = (
+  async def test_set_attr(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('.product_pod h3')
         .set({'url': 'a::attr(href)',
               'title': 'a::text'})
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(20, len(data))
@@ -193,8 +193,8 @@ class TestHypotonic(aiounittest.AsyncTestCase):
                    'url': 'http://books.toscrape.com/catalogue/sharp-objects_997/index.html'},
                   data)
 
-  def test_then(self):
-    data, errors = (
+  async def test_then(self):
+    data, errors = await (
       Hypotonic()
         .get('http://testing-ground.scraping.pro/textlist', {'ver': '3'})
         .find('h1')
@@ -203,32 +203,32 @@ class TestHypotonic(aiounittest.AsyncTestCase):
             context,
             {**data, 'version': context.url.query['ver']}
         ))
-        .data()
+        .run()
     )
 
     self.assertFalse(errors)
     self.assertEqual(1, len(data))
     self.assertIn({'title': 'TEXT LIST (version 3)', 'version': '3'}, data)
 
-  def test_follow(self):
-    data, errors = (
+  async def test_follow(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .follow('.product_pod h3 a::attr(href)')
         .find('h1')
         .set('title')
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(20, len(data))
     self.assertIn({'title': 'Sharp Objects'}, data)
 
-  def test_paginate(self):
-    data, errors = (
+  async def test_paginate(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .paginate('.next a::attr(href)', 3)
         .find('li article')
         .set({'title': 'h3 a'})
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(60, len(data))
@@ -236,67 +236,67 @@ class TestHypotonic(aiounittest.AsyncTestCase):
     self.assertIn({'title': 'In Her Wake'}, data)
     self.assertIn({'title': 'Thirst'}, data)
 
-  def test_filter(self):
-    data, errors = (
+  async def test_filter(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('li article')
         .filter('.Five')
         .set({'title': 'h3 a'})
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(4, len(data))
     self.assertIn({'title': 'Set Me Free'}, data)
     self.assertNotIn({'title': 'Sharp Objects'}, data)
 
-  def test_match(self):
-    data, errors = (
+  async def test_match(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('.product_pod h3 a')
         .match('[tT]he')
         .set('title')
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(9, len(data))
     self.assertIn({'title': 'Tipping the Velvet'}, data)
     self.assertNotIn({'title': 'Sharp Objects'}, data)
 
-  def test_match_case_insensitive(self):
+  async def test_match_case_insensitive(self):
     import re
 
-    data, errors = (
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('.product_pod h3 a')
         .match('THE', flags=re.IGNORECASE)
         .set('title')
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(9, len(data))
     self.assertIn({'title': 'Tipping the Velvet'}, data)
     self.assertNotIn({'title': 'Sharp Objects'}, data)
 
-  def test_log(self):
+  async def test_log(self):
     with self.assertLogs(logger, level='INFO') as context:
-      data, errors = (
+      data, errors = await (
         Hypotonic('http://books.toscrape.com/')
           .find('.h1 a')
           .set('title')
           .log()
-          .data())
+          .run())
 
     self.assertFalse(errors)
     self.assertIn(
       'INFO:hypotonic:1:<a href="http://books.toscrape.com/index.html">Books to Scrape</a>',
       context.output)
 
-  def test_extracted_text_is_stripped(self):
-    data, errors = (
+  async def test_extracted_text_is_stripped(self):
+    data, errors = await (
       Hypotonic('http://books.toscrape.com/')
         .find('.nav-list a')
         .set('category')
-        .data())
+        .run())
 
     self.assertFalse(errors)
     self.assertEqual(51, len(data))
@@ -304,12 +304,12 @@ class TestHypotonic(aiounittest.AsyncTestCase):
       self.assertEqual(item['category'], item['category'].strip())
 
   @vcr.use_cassette()
-  def test_br_tags_become_newlines(self):
-    data, errors = (
+  async def test_br_tags_become_newlines(self):
+    data, errors = await (
       Hypotonic('http://testing-ground.scraping.pro/blocks')
         .find('.best')
         .set('text')
-        .data()
+        .run()
     )
 
     self.assertFalse(errors)
@@ -318,12 +318,12 @@ class TestHypotonic(aiounittest.AsyncTestCase):
     self.assertIn({'text': 'BEST\nPRICE!'}, data)
 
   @vcr.use_cassette()
-  def test_utf_8_is_canonicalized(self):
-    data, errors = (
+  async def test_utf_8_is_canonicalized(self):
+    data, errors = await (
       Hypotonic('https://dennikn.sk/kontakt/')
         .find('main article div p:nth-child(2)')
         .set('text')
-        .data()
+        .run()
     )
 
     self.assertFalse(errors)
@@ -331,26 +331,26 @@ class TestHypotonic(aiounittest.AsyncTestCase):
     self.assertEqual(data[0]['text'],
                      'Ak nájdete chybu v článkoch, budeme vďační, ak nám napíšete na editori@dennikn.sk.')
 
-  def test_get_json(self):
-    data, errors = (
+  async def test_get_json(self):
+    data, errors = await (
       Hypotonic('https://jsonplaceholder.typicode.com/users')
         .find('$[*]')
         .set({'id': '$.id',
               'name': '$.name'})
-        .data()
+        .run()
     )
 
     self.assertFalse(errors)
     self.assertEqual(10, len(data))
     self.assertIn({'id': 2, 'name': 'Ervin Howell'}, data)
 
-  def test_set_json_value(self):
-    data, errors = (
+  async def test_set_json_value(self):
+    data, errors = await (
       Hypotonic('https://jsonplaceholder.typicode.com/users')
         .find('$[*]')
         .find('$.address.geo')
         .set('coords')
-        .data()
+        .run()
     )
 
     self.assertFalse(errors)
@@ -358,8 +358,8 @@ class TestHypotonic(aiounittest.AsyncTestCase):
     self.assertIn({'coords': {'lat': '-37.3159', 'lng': '81.1496'}}, data)
 
   @vcr.use_cassette()
-  def test_non_standard_json_content_type(self):
-    data, errors = (
+  async def test_non_standard_json_content_type(self):
+    data, errors = await (
       Hypotonic()
         .get('https://www.sreality.cz/api/cs/v2/estates/count',
              {'category_main_cb': 1,
@@ -368,7 +368,7 @@ class TestHypotonic(aiounittest.AsyncTestCase):
               'locality_district_id': 5006,
               'locality_region_id': 10})
         .set({'size': '$.result_size'})
-        .data()
+        .run()
     )
 
     self.assertFalse(errors)
